b62f0c2fdb749bec52fa68ba0cd7decb368b85ef
sugat dhole <sugatdhole@gmail.com>
2025-04-12 19:08:35 +0530

    split string and summing the numbers

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -1,5 +1,12 @@
 class StringCalculator
     def self.add(input_string)
-        return 0
+        return 0 if input_string.empty?
+
+        numbers = input_string.split(',')
+        numbers = numbers.map do |number|
+            number.to_i
+        end
+
+        numbers.sum
     end
 end
