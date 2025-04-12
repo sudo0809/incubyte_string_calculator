f3d791cc456607fc0f40f3b398a04ce3e785ff0c
sugat dhole <sugatdhole@gmail.com>
2025-04-12 23:27:15 +0530

    test for multiple newlines with custom delimiter

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -14,8 +14,15 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
 
-    it 'supports custom single-character delimiters defined at the start' do
-      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    context 'with custom delimiter defined at start' do
+
+      it 'supports custom single-character delimiters' do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+
+      it 'handles multiple newlines between numbers' do
+        expect(StringCalculator.add("//;\n1;2\n3")).to eq(6)
+      end
     end
   end
 end
