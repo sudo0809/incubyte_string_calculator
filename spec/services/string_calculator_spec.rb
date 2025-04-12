1c6aaea25576167bba17d91a3a2c6b8b071024ba
sugat dhole <sugatdhole@gmail.com>
2025-04-12 19:32:23 +0530

    newline spec test added

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two numbers' do
       expect(StringCalculator.add('1,2')).to eq(3)
     end
+
+    it 'handles newlines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
