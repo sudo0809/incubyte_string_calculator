abd42e4898d0732db0b3aa303407526aa6492270
sugat dhole <sugatdhole@gmail.com>
2025-04-12 19:00:01 +0530

    test case added with a string containing two numbers and expected output

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
   end
 end
