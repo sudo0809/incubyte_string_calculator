d9bde553fcc0f830b2306b36f442793e13a748f5
sugat dhole <sugatdhole@gmail.com>
2025-04-12 19:37:42 +0530

    code to replace newline in string with a comma

--- app/services/string_calculator.rb
+++ app/services/string_calculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
     def self.add(input_string)
         return 0 if input_string.empty?
 
-        numbers = input_string.split(',')
+        numbers = input_string.gsub("\n", ',').split(',')
 
         numbers.map(&:to_i).sum
     end
